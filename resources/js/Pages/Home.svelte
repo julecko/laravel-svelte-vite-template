8017b9a1329dbfdeff918aadfcc30f18ca860d2d
Julecko <julkoklein@gmail.com>
2024-11-21 15:31:10 +0100

    Initial commit

---
+++ resources/js/Pages/Home.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let name: String = 'World';
+</script>
+
+<h1>Hello, {name}!</h1>
+
+<style lang="scss">
+    @use "../../scss/colors" as *;
+    h1 {
+        background-color: $primary-color;
+    }
+    p {
+        color: $text-color;
+    }
+</style>
